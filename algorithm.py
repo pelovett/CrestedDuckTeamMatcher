53282d9e782f54a4e52a7051b0a8f9360a810c60
pelovett <pelovett@gmail.com>
2017-04-14 11:31:51 -0700

    Added logic for possible team generation

--- algorithm.py
+++ algorithm.py
@@ -1,43 +1,61 @@
 import random
+import copy as cp
 
 class Algorithm:
 
-    def __init__(self, student_list, run_count=10):
+    def __init__(self, student_list, run_count=5, teamsize=3):
         self.students = student_list
         self.possible = []
+        #possible will hold different cofigurations based on indexes into self.students
+        self.team_size = teamsize
         self.iterations = run_count
+        self.large_teams = len(student_list) % teamsize
+        #The modulo of the teamsize is the number of +1 size teams
+        self.team_count = len(student_list) // teamsize - self.large_teams
+        #Total number of teams of the normal size
 
     def generate(self):
-        for i in range(run_count):
-            self.possible.append(rand_order(self.students))
+        for i in range(self.iterations):
+            self.possible.append(Algorithm.rand_order(self.students))
 
+    def score(self, config):
+        #Pass in possible ordering and return an int score for ordering
+        #Start by scoring all normal-sized teams
+        size = self.team_size
+        for index in range(team_count):
+            team = config[size*index:size*(index+1)]
+            #team is a list of the team member's indexes
+            for stud in team:
+                student = self.students[stud]
 
+    def print(self):
+        print("Raw students: ")
+        for student in self.students:
+            print(student)
+        print("Possibilities: ")
+        for team in self.possible:
+            print("----")
+            print(str(team))
+        print("Team size: " + str(self.team_size))
+
+        print("Number of normal teams: " + str(self.team_count))
+        print("Number of teams with +1 members: " + str(self.large_teams))
+
+    def set_team_size(self, newsize):
+        self.team_size = newsize
+        self.team_count = len(self.students) // newsize
+        self.team_count += 0 if len(self.students) % newsize else 1
 
     def rand_order(a_list):
         length = len(a_list)
-
+        temp_list = cp.deepcopy(a_list)
         for i in range(length):
             j = random.randint(i,length-1)
-            temp = a_list[i]
-            a_list[i] = a_list[j]
-            a_list[j] = temp
-
-        return a_list
-
-class Student:
-        #A class to wrap all data for a student
-        #Format:
-        #   name         = String with user name
-        #   email        = String of email address
-        #   schedule     = String of binary describing acceptable timeslots
-        #   skills       = String of ints describing comfort with skills
-        #   pref_partner = email of partner to match desired
-        #   pref_project = int describing desired project
+            temp = temp_list[i]
+            temp_list[i] = temp_list[j]
+            temp_list[j] = temp
         
-    def __init__(self, answer_dict):
-        self.name         = answer_dict["Name"]
-        self.email        = answer_dict["Email"]
-        self.schedule     = answer_dict["Schedule"]
-        self.skills       = answer_dict["Skills"]
-        self.pref_partner = answer_dict["Partners"]
-        self.pref_project = answer_dict["Project"]
+        return temp_list
+
+
+
