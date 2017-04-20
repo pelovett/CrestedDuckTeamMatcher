c33b0e84b64e9f8901d812dd1e47f904b8f3bf0c
Meme <meme@Wyatts-MacBook-Pro.local>
2017-04-19 19:52:34 -0700

    flask stuff

--- main.py
+++ main.py
@@ -8,9 +8,17 @@ import flask
 from flask import render_template
 from flask import request
 from flask import url_for
+from flask import make_response
+
+import io
+import csv
+
 
 import json
 import logging
+
+import algorithm
+from algorithm import Algorithm
 
 ###
 # Globals
@@ -30,6 +38,40 @@ def index():
   app.logger.debug("Main page entry")
   #TODO add variables to ajax
   return flask.render_template('index.html')
+
+def transform(text_file_contents):
+    return text_file_contents.replace("=", ",")
+
+@app.route('/result', methods=["POST"])
+def transform_view():
+    f = request.files['csv-file']
+    if not f:
+        return "No file"
+
+    csv_list = []
+
+    stream = io.StringIO(f.stream.read().decode("UTF8"), newline=None)
+    csv_input = csv.reader(stream)
+    #print("file contents: ", file_contents)
+    #print(type(file_contents))
+    print(csv_input)
+    for row in csv_input:
+        print(row)
+        csv_list.append(row)
+
+    stream.seek(0)
+    result = transform(stream.read())
+
+    #TODO TypeError: list indices must be integers or slices, not str
+    #something = Algorithm(csv_list).generate()
+    print(something)
+
+    response = make_response(result)
+
+    response.headers["Content-Disposition"] = "attachment; filename=result.csv"
+
+    #response.csv.html downloaded
+    return response
 
 @app.errorhandler(404)
 def page_not_found(error):
